d9de6e12b639ddbec2c637eb074a504541d13765
MohamedAbdalhalem <mohamed3ab7alem@gmail.com>
2025-07-14 17:37:53 +0300

    add search section

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,10 @@
-import { useSelector } from "react-redux"
-import { ourStoreType } from "../../lib/store"
-
+import SearchSection from "../../Components/SearchSection/SearchSection";
 
 export default function Home() {
-  const {token} = useSelector((state: ourStoreType) => state.authsilce)
+  
   return (
     <div>
-      {token}
+      <SearchSection/>
     </div>
   )
 }
